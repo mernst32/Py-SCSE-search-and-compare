42ba48b1b0a791d0a1b0671a3e6f3c97c81ffa88
Max Ernst <mernst32@yahoo.com>
2020-02-25 15:27:46 +0100

    refined error handling and progressbar;

--- dl_sc_code.py
+++ dl_sc_code.py
@@ -10,6 +10,11 @@ import argparse
 
 def handle_err(url, cause, src, id_num):
     # print("ERROR while downloading " + str(id_num) + " saving msg in " + str(id_num) + ".error")
+    try:
+        os.makedirs("err")
+        os.makedirs("err/{0}".format(src))
+    except FileExistsError as e:
+        pass
     with open("err/{0}/{1}.error".format(src, id_num), 'w', encoding='utf-8') as ofile:
         ofile.write(url + '\n' + repr(cause))
 
@@ -18,86 +23,108 @@ def get_raw(url):
     # print("get data from "+url)
     contents = urllib.request.urlopen(url).read()
     return json.loads(contents.decode('utf-8'))
+        
 
 
 def get_page(search, page, per_page, src):
     params = {'q': search, 'lan': '23', 'p': page, 'per_page': per_page, 'src': src["id"]}
     url = "https://searchcode.com/api/codesearch_I/?" + urllib.parse.urlencode(params)
-    raw_data = get_raw(url)
-    results = raw_data["results"]
-    id_list = []
-    for result in results:
-        id_list.append(result["id"])
-    # print("getting " + str(len(results)) + " items from page " + str(page))
-    for id_num in id_list:
-        url = "https://searchcode.com/api/result/" + str(id_num) + "/"
-        try:
-            code = get_raw(url)["code"]
-            lines = code.split('\n')
-            with open("out/{0}/{1}.java".format(src["source"], id_num), 'w', encoding='utf-8') as ofile:
-                ofile.write("// URL: " + url + '\n')
-                for line in lines:
-                    ofile.write(line + '\n')
-        except HTTPError as e:
-            handle_err(url, e, src["source"], id_num)
-        except json.decoder.JSONDecodeError as e:
-            handle_err(url, e, src["source"], id_num)
+    try:
+        raw_data = get_raw(url)
+        results = raw_data["results"]
+        id_list = []
+        for result in results:
+            id_list.append(result["id"])
+        # print("getting " + str(len(results)) + " items from page " + str(page))
+        for id_num in id_list:
+            url = "https://searchcode.com/api/result/" + str(id_num) + "/"
+            try:
+                code = get_raw(url)["code"]
+                lines = code.split('\n')
+                with open("out/{0}/{1}.java".format(src["source"], id_num), 'w', encoding='utf-8') as ofile:
+                    ofile.write("// URL: " + url + '\n')
+                    for line in lines:
+                        ofile.write(line + '\n')
+            except HTTPError as e:
+                handle_err(url, e, src["source"], id_num)
+            except json.decoder.JSONDecodeError as e:
+                handle_err(url, e, src["source"], id_num)
+        return len(id_list)
+    except HTTPError as e:
+        print("ERROR:Could not get data from " + url + ":" + repr(e))
+        return 0
 
 
 def get_java_code_from_repo(search, src, per_page):
     params = {'q': search, 'lan': '23', 'src': src["id"]}
     url = "https://searchcode.com/api/codesearch_I/?" + urllib.parse.urlencode(params)
-    raw_data = get_raw(url)
-    total = raw_data["total"]
-    pages = int(math.ceil(total / per_page))
-    bar_len = 50
-    for page in range(0, pages):
-        get_page(search, page, per_page, src)
-
-        prog = int(((page + 1) * bar_len) // pages)
-        bar = '#' * prog + '.' * (bar_len - prog)
-        print("Downloading from " + src["source"] + ": " + "[" + bar + "] " + str(int((prog / bar_len) * 100)) + "%",
-              end='\r')
-    print()
+    try:
+        raw_data = get_raw(url)
+        total = raw_data["total"]
+        if total > (50 * per_page):
+            total = (50 * per_page)
+        pages = int(math.ceil(total / per_page))
+        bar_len = 50
+        dl_size = 0
+        print("Downloading from " + src["source"] + ": ")
+        for page in range(0, pages):
+            dl_size = dl_size + get_page(search, page, per_page, src)
+            
+            if dl_size == 0:
+                print("Nothing to download!")
+            else:
+                prog = int(((page + 1) * bar_len) // pages)
+                bar = '#' * prog + '.' * (bar_len - prog)
+                print("\t" + str(int((prog / bar_len) * 100)) + "%" + " [" + bar + "] " 
+                    + str(dl_size) + "/" + str(total) + " Downloaded", 
+                    end='\r')
+            time.sleep(1)
+        print()
+    except HTTPError as e:
+        print("ERROR:Could not get data from " + url + ":" + repr(e))
 
 
 def get_java_code(search, info, repo, per_page):
     params = {'q': search, 'lan': '23'}
     url = "https://searchcode.com/api/codesearch_I/?" + urllib.parse.urlencode(params)
-    raw_data = get_raw(url)
-    src_filters = raw_data["source_filters"]
-    print("Found " + str(len(src_filters)) + " repo-source(s) with java files, that contain " 
-          + "the string \"" + search + "\"." + '\n')
-    if not info:
-        try:
-            os.makedirs("out")
-            os.makedirs("err")
-        except FileExistsError as e:
-            pass
-        print("Starting download of the Java files...")
-        if repo == -1:
-            for src in src_filters:
-                try:
-                    os.makedirs("out/{0}".format(src["source"]))
-                    os.makedirs("err/{0}".format(src["source"]))
-                except FileExistsError as e:
-                    pass
-                get_java_code_from_repo(search, src, per_page)
-                time.sleep(5)
-        else:
-            for src in src_filters:
-                if src["id"] == repo:
+    try:
+        raw_data = get_raw(url)
+        src_filters = raw_data["source_filters"]
+        print("Found " + str(len(src_filters)) + " repo-source(s) with java files, that contain " 
+            + "the string \"" + search + "\"." + '\n')
+        if not info:
+            try:
+                os.makedirs("out")
+            except FileExistsError as e:
+                pass
+            print("Starting download of the Java files...")
+            if repo == -1:
+                for src in src_filters:
                     try:
                         os.makedirs("out/{0}".format(src["source"]))
-                        os.makedirs("err/{0}".format(src["source"]))
                     except FileExistsError as e:
                         pass
                     get_java_code_from_repo(search, src, per_page)
-        print("DONE WITH DOWNLOADS!")
-    else:
-        for src in src_filters:
-            print(src["source"] + "[repo_id:" + str(src["id"]) + "]" + " with a total of " + str(src["count"])
-                  + " result(s).")
+                    time.sleep(2)
+            else:
+                for src in src_filters:
+                    if src["id"] == repo:
+                        try:
+                            os.makedirs("out/{0}".format(src["source"]))
+                        except FileExistsError as e:
+                            pass
+                        get_java_code_from_repo(search, src, per_page)
+            print("DONE WITH DOWNLOADS!")
+        else:
+            for src in src_filters:
+                print(src["source"] + "[repo_id:" + str(src["id"]) + "]" + " with a total of " + str(src["count"])
+                    + " result(s).")
+                if src["count"] > (50 * per_page):
+                    print("WARNING:The searchcode API only allows the download from up to 50 pages!")
+                    print("\tSo this script will only be able to get " + str(50 * per_page) + " of the "
+                            + str(src["count"]) + " files!")
+    except HTTPError as e:
+        print("ERROR:Could not get data from " + url + ":" + repr(e))
 
 
 parser = argparse.ArgumentParser(
